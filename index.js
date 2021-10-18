a1c0924ce482e951de7a9360ce04d7c0da5f9fa6
Christian <cespinoza8001@gmail.com>
2021-10-18 17:16:52 -0400

    commit tree eb25acbc9a2953efce39f295f724e1407c56b8e6

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", () => {
+    
+    const firstPrg = document.querySelector('#text');
+    firstPrg.textContent = "This is really cool!";
+    console.log(firstPrg.textContent);
+
+})
